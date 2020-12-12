38b75a8eb48cf47dc796aaeacb80cebfd3edaf35
lkm008 <1524117229@qq.com>
2020-12-12 19:12:11 +0800

    D4C1.页面静态化：模板管理2.GridFS存取测试

---
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/GridFsTemplateTest.java
@@ -0,0 +1,33 @@
+package com.xuecheng.manage_cms;
+
+import org.bson.types.ObjectId;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GridFsTemplateTest {
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Test
+    public void testGridFs() throws FileNotFoundException {
+        //要存储的文件
+        File file = new File("H:/Program/code/pj/xuechengzaixian/code/qianduan/xc-ui-pc-static-portal/include/index_banner.html");
+        //定义输入流
+        FileInputStream inputStram = new FileInputStream(file);
+        //向GridFS存储文件
+        ObjectId objectId = gridFsTemplate.store(inputStram, "轮播图测试文件01", "");
+        //得到文件ID
+        String fileId = objectId.toString();
+        System.out.println(fileId);//5fd4a0a8a513c22848469f89
+    }
+}
