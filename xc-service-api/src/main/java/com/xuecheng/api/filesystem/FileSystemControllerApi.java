0539c397ac3a3f499973cfda1be419f47a97d622
lkm008 <1524117229@qq.com>
2020-12-17 14:28:06 +0800

    D8:文件系统管理：1上传

---
+++ xc-service-api/src/main/java/com/xuecheng/api/filesystem/FileSystemControllerApi.java
@@ -0,0 +1,22 @@
+package com.xuecheng.api.filesystem;
+
+import com.xuecheng.framework.domain.filesystem.response.UploadFileResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.multipart.MultipartFile;
+@Api(value="文件系统管理接口",description = "文件管理接口，提供文件的上传和下载")
+public interface FileSystemControllerApi {
+    /**
+     * 上传文件
+     * @param multipartFile 文件
+     * @param filetag 文件标签
+     * @param businesskey 业务key
+     * @param metadata 元信息,json格式
+     * @return
+     */
+    @ApiOperation("文件上传")
+    public UploadFileResult upload(MultipartFile multipartFile,
+                                   String filetag,
+                                   String businesskey,
+                                   String metadata);
+}
