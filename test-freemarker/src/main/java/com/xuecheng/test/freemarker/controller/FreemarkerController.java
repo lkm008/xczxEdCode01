ffe14ce173e27d294dcbd1db8c3c9e0420ebb9e7
lkm008 <1524117229@qq.com>
2020-12-18 20:01:21 +0800

    D9：使用test-freekmarker模块测试课程页面生成

--- test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
+++ test-freemarker/src/main/java/com/xuecheng/test/freemarker/controller/FreemarkerController.java
@@ -101,5 +101,12 @@ public class FreemarkerController {
         return "test2";
     }
 
-
+    //课程详情页面测试
+    @RequestMapping("/course")
+    public String course(Map<String,Object> map){
+        ResponseEntity<Map> forEntity = restTemplate.getForEntity("http://localhost:31200/course/courseview/4028e581617f945f01617f9dabc40000", Map.class);
+        Map body = forEntity.getBody();
+        map.putAll(body);
+        return "course";
+    }
 }
---
+++ test-freemarker/src/test/java/com/xuecheng/test/freemarker/controller/FreemarkerControllerTest.java
@@ -0,0 +1,16 @@
+package com.xuecheng.test.freemarker.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class FreemarkerControllerTest {
+@Autowired
+FreemarkerController freemarkerController;
+
+}
