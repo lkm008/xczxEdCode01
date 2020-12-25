cf3a5d7a2e85e7f96018b43d0bb41d351c62066f
lkm008 <1524117229@qq.com>
2020-12-26 00:26:36 +0800

    D14：课程计划和媒资关联-查询视频信息

--- xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TeachplanMapperTest.java
+++ xc-service-manage-course/src/test/java/com/xuecheng/manage_course/dao/TeachplanMapperTest.java
@@ -19,13 +19,18 @@ TeachplanMapper teachplanMapper;
         TeachplanNode teachplanNode = teachplanMapper.selectList("402885816243d2dd016243f24c030002");
         System.out.println("id1 = " + teachplanNode.getId());
         System.out.println("pname1 = " + teachplanNode.getPname());
+        System.out.println("grade1 = " + teachplanNode.getGrade());
         List<TeachplanNode> children = teachplanNode.getChildren();
         for (TeachplanNode teachplanNode2:children) {
             System.out.println("id2 = " + teachplanNode2.getId());
             System.out.println("pname2 = " + teachplanNode2.getPname());
+            System.out.println("grade2 = " + teachplanNode2.getGrade());
             for (TeachplanNode teachplanNode3:teachplanNode2.getChildren()) {
                 System.out.println("id3 = " + teachplanNode3.getId());
                 System.out.println("pname3 = " + teachplanNode3.getPname());
+                System.out.println("grade3 = " + teachplanNode3.getGrade());
+                System.out.println("mediaId = " + teachplanNode3.getMediaId());
+                System.out.println("mediaFileOriginalName3 = " + teachplanNode3.getMediaFileOriginalName());
             }
         }
 
