9c7c335c6ecb080df97b31bec77a3be480f10854
lkm008 <1524117229@qq.com>
2020-12-27 00:13:49 +0800

    D15:媒资管理集成 2.4 搜索服务查询课程媒资接口

--- xc-service-search/src/main/java/com/xuecheng/search/web/controller/EsCourseController.java
+++ xc-service-search/src/main/java/com/xuecheng/search/web/controller/EsCourseController.java
@@ -2,8 +2,10 @@ package com.xuecheng.search.web.controller;
 
 import com.xuecheng.api.course.EsCourseControllerApi;
 import com.xuecheng.framework.domain.course.CoursePub;
+import com.xuecheng.framework.domain.course.TeachplanMediaPub;
 import com.xuecheng.framework.domain.search.CourseSearchParam;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.search.service.EsCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,5 +32,22 @@ public class EsCourseController implements EsCourseControllerApi {
     @GetMapping("/getall/{id}")
     public Map<String, CoursePub> getall(@PathVariable("id") String id)  {
         return esCourseService.getall(id);
+    }
+
+    @Override
+    @GetMapping(value="/getmedia/{teachplanId}")
+    public TeachplanMediaPub getmedia(@PathVariable("teachplanId") String teachplanId) {
+        //将课程计划id放在数组中，为调用service作准备
+        String[] teachplanIds = new String[]{teachplanId};
+        //通过service查询ES获取课程媒资信息
+        QueryResponseResult mediaPubQueryResponseResult = esCourseService.getmedia(teachplanIds);
+        QueryResult<TeachplanMediaPub> queryResult = mediaPubQueryResponseResult.getQueryResult();
+        if(queryResult!=null
+                && queryResult.getList()!=null
+                && queryResult.getList().size()>0){
+            //返回课程计划对应课程媒资
+            return queryResult.getList().get(0);
+        }
+        return new TeachplanMediaPub();
     }
 }
