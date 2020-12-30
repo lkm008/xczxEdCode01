242a7f16e41f476d04e0ec1e3028a821b7f4596e
lkm008 <1524117229@qq.com>
2020-12-30 18:05:30 +0800

    D17配置zuul网关访问课程资源测试-添加自定义过滤器

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -61,7 +61,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 //下边的路径放行
                 .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
                         "/swagger-resources","/swagger-resources/configuration/security",
-                        "/swagger-ui.html","/course/coursepic/list/*")
+                        "/swagger-ui.html","/course/coursepic/list/*","/course/coursebase/get/*")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
